5df49a607a4919eb296040c9c72591fffe572893
auqid <38567390+auqid@users.noreply.github.com>
2023-09-03 16:25:02 +0530

    added button for opening and closing form

--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [isEditing, setEditing] = useState(false);
   const onSaveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -10,11 +11,25 @@ const NewExpense = (props) => {
     };
     console.log("from new expense", expenseData);
     props.onSaveExpense(expenseData);
+    setEditing(false);
   };
-
+  const startEditinghandler = () => {
+    setEditing(true);
+  };
+  const stopEditingHandler = () => {
+    setEditing(false);
+  };
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler}></ExpenseForm>
+      {!isEditing && (
+        <button onClick={startEditinghandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={onSaveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        ></ExpenseForm>
+      )}
     </div>
   );
 };
